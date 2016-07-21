d848baf25eda1c08c549fd21eaacf88b0a26cec2
Solulink-AD <shpark@solulink.co.kr>
2016-07-21 16:32:32 +0900

    initial upload

---
+++ kairos-monitor-extension/src/main/java/kr/co/soluink/appdynamics/extension/config/Configuration.java
@@ -0,0 +1,60 @@
+package kr.co.soluink.appdynamics.extension.config;
+
+public class Configuration {
+
+    private String host;
+    private int port;
+    private String sid;
+    private String username;
+    private String password;
+
+    private String metricPathPrefix;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMetricPathPrefix() {
+        return metricPathPrefix;
+    }
+
+    public void setMetricPathPrefix(String metricPathPrefix) {
+        this.metricPathPrefix = metricPathPrefix;
+    }
+}
